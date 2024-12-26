90024a1b8b62cc19e7d6125dee45c66a10955b72
codingxsensei <codingxsensei@gmail.com>
2024-12-26 22:10:01 +0530

    Create factorial.py

---
+++ factorial/factorial.py
@@ -0,0 +1,8 @@
+# Assign a value to n for which factorial is to be calculated
+n = 5
+factorial = 1
+# Loop from 1 to n
+for i in range(1, n + 1):
+    factorial *= i
+# Print the factorial
+print("Factorial:", factorial)
